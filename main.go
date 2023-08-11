11003e3fac09e85cc2ecb1c8c3fdc01eb8b09960
devsunset <devsunset@gmail.com>
2023-08-11 16:51:50 +0900

    dev

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host 193.123.252.22
+// @host 144.24.71.73
 // @BasePath /
 func main() {
 
